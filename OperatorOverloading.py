df02a67da0fc8b3b6794ae52ec1dc1a23ce6206a
Nihal Sayyad <60935072+NihalSayyad@users.noreply.github.com>
2020-08-08 20:56:57 +0530

    Add files via upload

---
+++ OperatorOverloading.py
@@ -0,0 +1,37 @@
+
+class Student:
+
+    def __init__(self,m1,m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def __add__(self, other):   # overloading add operator
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        s3 = Student(m1,m2)
+
+        return s3
+
+    def __gt__(self, other):    # oveloading greater than operator
+        s1 = self.m1 + self.m2
+        s2 = other.m1 + other.m2
+
+        if s1 > s2:
+            return True
+        else:
+            return False
+
+
+
+s1 = Student(58,69)
+s2 = Student(60,65)
+
+s3 = s1 + s2 # -> convertd into Student.__add__(s1,s2)
+
+print(s3.m1)
+
+if s1 > s2:  # -> converted into Student.__gt__(s1,s2)
+    print("S1 wins")
+else:
+    print("s2 wins")
+
