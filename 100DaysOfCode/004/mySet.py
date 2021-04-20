8a45b1e5c44c4dccf320b26cc2dfc249c1c89a3e
NihalSayyad <nihalsayyad912@gmail.com>
2021-04-20 20:52:36 +0530

    MultiSet with the help of Counter

--- 100DaysOfCode/004/mySet.py
+++ 100DaysOfCode/004/mySet.py
@@ -4,4 +4,12 @@ b = {4,5,6}
 print(a.intersection(b))
 print(a.union(b))
 print(a.difference(b))
- 
+
+
+#multi set operations
+#Counter from collection will not keep ordering, but keeps the count
+from collections import Counter
+
+counterA = Counter(['a','b', 'c','b'])
+
+print(counterA)
