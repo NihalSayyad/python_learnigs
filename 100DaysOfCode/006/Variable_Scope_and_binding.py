126340896789bec795440dd6a5426f90957dafc9
NihalSayyad <nihalsayyad912@gmail.com>
2021-04-22 21:24:08 +0530

    scope and binding

--- 100DaysOfCode/006/Variable_Scope_and_binding.py
+++ 100DaysOfCode/006/Variable_Scope_and_binding.py
@@ -23,3 +23,40 @@ def change_global_x():
     print(x)
 change_global_x() # prints Bye
 print(x) # prints Bye
+
+#locals() globals()
+
+print(globals().keys())
+print(locals().keys())
+
+print('--------------------------------------------------------------------------------')
+
+def f1():
+    bar = 1
+    def f2():
+        baz = 2
+        # here, foo is a global variable, baz is a local variable
+        # bar is not in either scope
+        print(locals().keys()) # ['baz']
+        print('bar' in locals()) # False
+        print('bar' in globals()) # False
+        return f3()
+ 
+    def f3():
+        baz = 3
+        print(bar) # bar from f1 is referenced so it enters local scope of f3 (closure)
+        print(locals().keys()) # ['bar', 'baz']
+        print('bar' in locals()) # True
+        print('bar' in globals()) # False
+        return f4()
+
+    def f4():
+        bar = 4 # a new local bar which hides bar from local scope of f1
+        baz = 4
+        print(bar)
+        print(locals().keys()) # ['bar', 'baz']
+        print('bar' in locals())
+
+    return f2()
+
+print(f1())
