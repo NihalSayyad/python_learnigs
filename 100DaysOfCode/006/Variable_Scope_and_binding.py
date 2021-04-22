65060f05d671d608e6da52a868f8776151108488
NihalSayyad <nihalsayyad912@gmail.com>
2021-04-22 21:07:48 +0530

    global variables

--- 100DaysOfCode/006/Variable_Scope_and_binding.py
+++ 100DaysOfCode/006/Variable_Scope_and_binding.py
@@ -13,3 +13,13 @@ c= counter()
 print(c())
 print(c())
 print(c())
+
+#global variables
+#Assignment will happen at the modules's top level, not programs
+x = 'Hi'
+def change_global_x():
+    global x
+    x = 'Bye'
+    print(x)
+change_global_x() # prints Bye
+print(x) # prints Bye
