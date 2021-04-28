3842726e4385b5e24383521c939e1c1b44d831c8
NihalSayyad <nihalsayyad912@gmail.com>
2021-04-28 20:49:17 +0530

    Day11

---
+++ 100DaysOfCode/011/Functions.py
@@ -0,0 +1,9 @@
+#Function without arguments
+def generate_full_name():
+    first_name = 'Nihal'
+    last_name = 'Sayyad'
+
+    full_name = first_name + ' ' + last_name
+    print(full_name)
+
+generate_full_name()
