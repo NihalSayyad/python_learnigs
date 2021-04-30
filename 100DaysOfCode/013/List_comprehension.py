0007b6d657522b9fe6f8f78f60590dea4888f844
NihalSayyad <nihalsayyad912@gmail.com>
2021-04-30 22:06:55 +0530

    List Comprehension

--- 100DaysOfCode/013/List_comprehension.py
+++ 100DaysOfCode/013/List_comprehension.py
@@ -3,3 +3,11 @@ lst = list(word)
 
 print(lst)
 
+numbers = [i for i in range(11)]
+print(numbers)
+
+squares = [i * i for i in range(11)]
+print(squares)
+
+numbers = [(i, i * i) for i in range(11)]
+print(numbers)
