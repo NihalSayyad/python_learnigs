faa1ad830dbe2955b6a0858baedf036127148508
NihalSayyad <nihalsayyad912@gmail.com>
2021-05-03 21:12:46 +0530

    decorators with arguments

--- 100DaysOfCode/015/Decorators_in_py.py
+++ 100DaysOfCode/015/Decorators_in_py.py
@@ -1,9 +1,9 @@
 
 def decorator_function(original_function):
 
-    def wrapper_function():
+    def wrapper_function(*args, **kwargs):
         print("This is from wrapper function")
-        return original_function()
+        return original_function(*args, **kwargs)
     return wrapper_function
 
 def display():
@@ -19,5 +19,10 @@ decorated_func()
 def display2():
     print('This is from Display 2')
 
+@decorator_function
+def display3(name, age):
+    print(f'This is from Display 3 with ({name} ,{age})')
+
+display3('Nihal', 26)
 
 display2()
