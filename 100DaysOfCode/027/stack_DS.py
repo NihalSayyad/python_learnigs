449c463ac3ebb6ad68f41932dd8d2868ec7df0b6
NihalSayyad <nihalsayyad912@gmail.com>
2021-05-27 20:44:34 +0530

    stack DS

--- 100DaysOfCode/027/stack_DS.py
+++ 100DaysOfCode/027/stack_DS.py
@@ -8,11 +8,23 @@ class Stack():
     def pop(self):
         self.items.pop()
 
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+        return self.items[-1]
+
     def get_stack(self):
         return self.items
 
 s = Stack()
 s.push('A')
 s.push('B')
-
+s.push('C')
+s.push('D')
 print(s.get_stack())
+
+print(s.peek())
