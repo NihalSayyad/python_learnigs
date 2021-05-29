10a1cbeab663589ecc00f87030dd9e13d0350edf
NihalSayyad <nihalsayyad912@gmail.com>
2021-05-29 19:42:14 +0530

    day029

---
+++ 100DaysOfCode/029/LinkedList.py
@@ -0,0 +1,34 @@
+#Linked List (Singly Linked List)
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head == None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data, end="->")
+            curr_node = curr_node.next
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.print_list()
