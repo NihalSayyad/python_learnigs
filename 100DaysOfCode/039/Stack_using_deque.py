8f09700a7c22bee290570d606d88eec9c0729c87
NihalSayyad <nihalsayyad912@gmail.com>
2021-06-16 20:35:28 +0530

    day039 -- stack using deque

---
+++ 100DaysOfCode/039/Stack_using_deque.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def push(self, data):
+        self.container.append(data)
+
+    def pop(self):
+        return self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+    def is_empty(self):
+        return len(self.container) == 0
+
+    def size(self):
+        return len(self.container)
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+print(stack.container)
+print(stack.peek())
+print(stack.size())
+print(stack.pop())
+print(stack.peek())
+print(stack.size())
+
