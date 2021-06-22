ca30144521776da8ceed7b0e05acfedca3b7bcaa
NihalSayyad <nihalsayyad912@gmail.com>
2021-06-22 21:19:42 +0530

    day042 -- Multiprocessing and data sharing between processes using Array and Value

--- 100DaysOfCode/042/Multi_proc_data_sharing.py
+++ 100DaysOfCode/042/Multi_proc_data_sharing.py
@@ -2,17 +2,21 @@ import multiprocessing
 
 result = []
 
-def calc_square(numbers):
-    global result
-    for n in numbers:
-        result.append(n*n)
-    print("Inside the process: result : ", str(result))
+def calc_square(numbers, result, v):
+    v.value = 9.99
+    for idx, n in enumerate(numbers):
+        result[idx] = n*n
+
 
 if __name__ == '__main__':
     numbers = [2,3,4,5]
-    p = multiprocessing.Process(target=calc_square, args=(numbers,))
+    result = multiprocessing.Array('i', 4)
+    v = multiprocessing.Value('d', 0.0)
+    p = multiprocessing.Process(target=calc_square, args=(numbers,result,v))
 
     p.start()
     p.join()
 
-    print('Outside process: result : ', str(result))
+    print(result[:])
+    print(v)
+    print(v.value)
