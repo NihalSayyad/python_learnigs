de99e89cd7abe04ea6ac93343d46a5fbf886946f
NihalSayyad <nihalsayyad912@gmail.com>
2021-06-23 20:57:09 +0530

    day043 - Multiproc pool - map-reduce

--- 100DaysOfCode/043/Multiproc_pool.py
+++ 100DaysOfCode/043/Multiproc_pool.py
@@ -2,20 +2,21 @@ from multiprocessing import Pool
 import time
 
 def f(n):
-    time.sleep(0.5)
-    return n*n
+    sum = 0
+    for x in range(1000):
+        sum += x*x
+    return sum
 
 if __name__ == '__main__':
-    array = [1,2,3,4,5,6,7,8,9,10]
-    t = time.time()
+    t1 = time.time()
     p = Pool()
-    result = p.map(f, array)
-    print(result)
-    print("Done in ", str(time.time() - t))
+    result = p.map(f, range(100000))
+    p.close()
+    p.join()
+    print("Done in ", str(time.time() - t1))
 
-    t= time.time()
+    t2 = time.time()
     result = []
-    for i in array:
+    for i in range(100000):
         result.append(f(i))
-    print(result)
-    print("Done in ", str(time.time() - t))
+    print("Done in ", str(time.time() - t2))
