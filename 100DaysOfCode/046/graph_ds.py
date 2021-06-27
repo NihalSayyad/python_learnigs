52faff2346ba5ba407d4dda16dd888c2ac994c4b
NihalSayyad <nihalsayyad912@gmail.com>
2021-06-27 21:32:03 +0530

    day046 - graph ds - get all paths

--- 100DaysOfCode/046/graph_ds.py
+++ 100DaysOfCode/046/graph_ds.py
@@ -9,6 +9,22 @@ class Graph:
                 self.graph_dict[start] = [end]
         print(self.graph_dict)
 
+    def get_paths(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+
+        if start not in self.graph_dict:
+            return []
+
+        paths = []
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths = self.get_paths(node, end, path)
+                for p in new_paths:
+                    paths.append(p)
+        return paths
 
 if __name__ == '__main__':
     routes = [
@@ -21,3 +37,7 @@ if __name__ == '__main__':
     ]
 
     route_graph = Graph(routes)
+    print(route_graph.get_paths("Mumbai", "Mumbai"))
+    print(route_graph.get_paths("Toronto", "Mumbai"))
+    print(route_graph.get_paths("Mumbai", "New York"))
+    print(route_graph.get_paths("Mumbai", "Toronto"))
