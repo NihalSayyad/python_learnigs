b9186458dca905674df893dcbf827972449fefb0
NihalSayyad <nihalsayyad912@gmail.com>
2021-07-10 13:06:26 +0530

    day046 - graph ds - Flood fill - print matrix

--- 100DaysOfCode/050/flood_fill.py
+++ 100DaysOfCode/050/flood_fill.py
@@ -1,4 +1,17 @@
+def printMat(mat):
+    for i in mat:
+        for j in i:
+            print(j, end="")
+        print("\n", end="")
+
 
 
 if __name__ == '__main__':
-    
+    mat = []
+    with open('apple.txt', 'r') as f:
+        line = f.readline().split()
+        R, C = int(line[0]), int(line[1])
+        for i in range(R):
+            mat.append(list(f.readline()))
+
+    printMat(mat)
