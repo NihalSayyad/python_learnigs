814389f5cbb7313642651a9aa01b7bf86890f4d3
NihalSayyad <nihalsayyad912@gmail.com>
2021-07-10 13:11:37 +0530

    day046 - graph ds - Flood fill

--- 100DaysOfCode/050/flood_fill.py
+++ 100DaysOfCode/050/flood_fill.py
@@ -4,7 +4,16 @@ def printMat(mat):
             print(j, end="")
         print("\n", end="")
 
+def floodFill(mat, i, j, ch, color):
+    if i<0 or j<0 or i>=R or j>=C or mat[i][j] !=ch:
+        return
 
+    mat[i][j] = color
+
+    floodFill(mat, i+1, j, ch, color)
+    floodFill(mat, i, j+1, ch, color)
+    floodFill(mat, i-1, j, ch, color)
+    floodFill(mat, i, j-1, ch, color)
 
 if __name__ == '__main__':
     mat = []
@@ -14,4 +23,7 @@ if __name__ == '__main__':
         for i in range(R):
             mat.append(list(f.readline()))
 
+    printMat(mat)
+
+    floodFill(mat, 8, 13, '.', 'R')
     printMat(mat)
