0266e9eb2285c9303bdb1e441fc9b846b5cc8be6
NihalSayyad <nihalsayyad912@gmail.com>
2021-07-18 19:15:01 +0530

    binary search

--- 100DaysOfCode/050/binary_search_in_py.py
+++ 100DaysOfCode/050/binary_search_in_py.py
@@ -2,7 +2,6 @@ import time
 
 def linear_search(numbers, number_to_find):
     for index, element in enumerate(numbers):
-        time.sleep(1)
         if element == number_to_find:
             return index
 
@@ -14,7 +13,6 @@ def binary_search(numbers, number_to_find):
     last_index = len(numbers) - 1
 
     while left_index <= last_index:
-        time.sleep(1)
         mid_index = (left_index + last_index) // 2
         mid_number = numbers[mid_index]
 
@@ -28,6 +26,22 @@ def binary_search(numbers, number_to_find):
 
     return -1
 
+def binary_search_recursive(number_list, number_to_find, left_index, right_index):
+    mid = (left_index + right_index + 1) // 2
+    if number_to_find == number_list[mid]:
+        return mid
+
+    if number_to_find < number_list[mid]:
+        right_index = mid-1
+        return binary_search_recursive(number_list, number_to_find, left_index, right_index)
+
+    if number_to_find > number_list[mid]:
+        left_index = mid + 1
+        return binary_search_recursive(number_list, number_to_find, left_index, right_index)
+
+    return -1
+
+
 if __name__ == "__main__":
     numbers = [12, 15, 17, 19, 21, 24, 45, 67, 68, 69, 72, 74, 78, 79, 81, 88, 89, 90, 91, 93, 95, 96, 99, 100]
     number_to_find = 100
@@ -37,4 +51,9 @@ if __name__ == "__main__":
 
     t1 = time.time()
     index = binary_search(numbers, number_to_find)
+    print(f"Number found at index {index} using binary search in {time.time() - t1} time")
+
+    t1 = time.time()
+    right_index = len(numbers) - 1
+    index = binary_search_recursive(numbers, number_to_find, 0, right_index)
     print(f"Number found at index {index} using binary search in {time.time() - t1} time")
